e36f31f598100f71343cb1aff0edca0cd3ddd4de
Michael Luebbers <quinteger75@gmail.com>
2022-04-14 10:58:52 -0400

    Finished tutorial part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,32 @@ module.exports = {
     title: `Michael Luebbers`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", 
-  "gatsby-plugin-sharp",
-    "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap", 
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         "icon": "src/images/icon.png"
       }
-    }, "gatsby-plugin-mdx", {
+    }, 
+    "gatsby-plugin-mdx", 
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "pages",
         "path": "./src/pages/"
+
       },
       __key: "pages"
-    }]
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },]
 };
+
