eaff9defd51faab56bb1f4a59c3528aa8d3be2bc
quinteger2 <ml3423@gmail.com>
2022-04-23 09:28:51 -0400

    Saved files this time

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 
@@ -70,59 +69,3 @@ export default Layout
         {children}
       </main>
     </div>*/
-=======
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle
-} from './layout.module.css'
-
-const Layout = ({ pageTitle, children }) => {
-
-  const data = useStaticQuery(graphql`query{
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }`)
-  
-  return (
-    <div className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
-  )
-}
-
-export default Layout
->>>>>>> e36f31f598100f71343cb1aff0edca0cd3ddd4de
